8f698a78ffe318ec3d0f0d52e0d6271fbba264b8
Jens Tinglev <tinglev@kth.se>
2020-10-06 16:26:16 +0200

    More commit

--- run.py
+++ run.py
@@ -1,18 +1,37 @@
 __author__ = 'tinglev@kth.se'
 
 import json
+from os import device_encoding
 import re
 import os
 import json
 import shutil
 import logging
 import tempfile
+import requests
 from urllib.parse import urlparse
 from azure.storage.blob import BlobServiceClient, BlobProperties, ContentSettings
 from flask import Flask, jsonify, request
+from flask_executor import Executor
 import environment
 import process
+
+def process_callback(future):
+    logger = logging.getLogger(__name__)
+    result = future.result()
+    if result[1]:
+        logger.debug('Error when generating report: %s', result[1])
+        return
+    logger.debug('Generated report for %s', result[0])
+    if 'callback' in result[0]:
+        logger.debug('Calling %s with deployment data', result[0]['callback'])
+        requests.post(result[0]['callback'], result[0])
+
 app = Flask(__name__)
+executor = Executor(app)
+executor.add_default_done_callback(process_callback)
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
 
 @app.route('/lighthouse-api/_monitor', methods=['GET'])
 def monitor():
@@ -21,9 +40,38 @@ def monitor():
 @app.route('/lighthouse-api', methods=['POST'])
 def create_report():
     logger = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    if not request.is_json:
+        return jsonify({'error': 'post is not json (bad content-type?)'}), 400
     req_json = request.get_json()
-    return jsonify('')
+    logger.debug('Got data: %s', json.dumps(req_json))
+    executor.submit(process_deployment, req_json)
+    return jsonify({'status': 'accepted'}), 202
+
+def process_deployment(deployment):
+    error = verify_deployment(deployment)
+    if error:
+        return deployment, error
+    for u in deployment['urls']:
+        process_url_to_scan(deployment, u)
+    return deployment, None
+
+def verify_deployment(deployment):
+    if 'application' not in deployment:
+        return '"application" missing from deployment json'
+    if 'commit' not in deployment:
+        return '"commit" missing from deployment json'
+    if 'environment' not in deployment:
+        return '"environment" missing from deployment json'
+    if 'urls' not in deployment:
+        return '"urls" missing from deployment json'
+    # callback is optional
+    #if 'callback' not in deployment:
+    #    return '"callback" missing from deployment json'
+    if 'team' not in deployment:
+        return '"team" missing from deployment json'
+    if not isinstance(deployment['urls'], list):
+        return '"urls" in deployment json is not a list'
+    return None
 
 def process_url_to_scan(deployment, url_to_scan):
     logger = logging.getLogger(__name__)
@@ -35,9 +83,8 @@ def process_url_to_scan(deployment, url_to_scan):
                                          f'-v {tmp_dir}:/report '
                                          f'{image}')
         logger.debug('Output from lighthouse was: "%s"', output)
-        app_name = deployment_util.get_application_name(deployment)
-        commit = deployment_util.get_application_version(deployment)
-        commit = commit.split('_')[1]
+        app_name = deployment['application']
+        commit = deployment['commit']
         url_path = urlparse(url_to_scan).path.replace('/', '-')
         report_path = f'{tmp_dir}/{app_name}_{commit}_{url_path}'
         os.rename(f'{tmp_dir}/report.report.html', f'{report_path}.html')
@@ -73,8 +120,8 @@ def upload_to_storage(deployment, report_path, url_path):
         try:
             blob_client.upload_blob(data)
             blob_client.set_http_headers(content_settings=blob_properties.content_settings)
-        except:
-            logger.debug('Couldnt upload file. Does it already exist?')
+        except Exception as ex:
+            logger.debug('Couldnt upload file. Does it already exist? Ex: %s', ex)
     filename = os.path.basename(json_path)
     blob_properties = get_blob_properties(filename)
     blob_client = client.get_blob_client(container=container, blob=blob_properties)
@@ -82,8 +129,8 @@ def upload_to_storage(deployment, report_path, url_path):
         try:
             blob_client.upload_blob(data)
             blob_client.set_http_headers(content_settings=blob_properties.content_settings)
-        except:
-            logger.debug('Couldnt upload file. Does it already exist?')
+        except Exception as ex:
+            logger.debug('Couldnt upload file. Does it already exist? Ex: %s', ex)
     logger.info('Report upload complete')
 
 def get_blob_properties(filename):
@@ -106,7 +153,7 @@ def get_blob_properties(filename):
 def clean_old_blobs(deployment, service_client, container_name, url_path):
     logger = logging.getLogger(__name__)
     client = service_client.get_container_client(container_name)
-    app_name = deployment_util.get_application_name(deployment)
+    app_name = deployment['application']
     blobs = client.list_blobs(name_starts_with=app_name)
     as_list = [
         b for b 
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+FLASK_APP=run.py poetry run flask run
